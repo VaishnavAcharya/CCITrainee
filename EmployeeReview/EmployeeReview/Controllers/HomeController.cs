365fcd3da1107dc6d930b4ccb8a68660b2c63233
VaishnavAcharya <vaishnavacharya@gmail.com>
2017-03-20 12:50:11 +0530

    Employee Review

---
+++ EmployeeReview/EmployeeReview/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeReview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RateEmployeeSkills(EmployeeModel emp)
+        {
+            
+            return View();
+        }
+        public ActionResult RateDevSkills(EmployeeModel emp)
+        {
+            return View("RateDevSkills",emp);
+        }
+
+        public ActionResult RateTechSkills(EmployeeModel emp)
+        {
+            return View("RateTechSkills",emp);
+        }
+
+        public ActionResult DevSkillType(EmployeeModel emp)
+        {
+            //switch (emp.EmpDevSkills)
+            //{
+            //    case EmployeeModel.DevSkills.CodingSkills:
+            //        emp.EmpDevSkills = EmployeeModel.DevSkills.QualityAssurance;
+            //        break;
+            //    case EmployeeModel.DevSkills.QualityAssurance:
+            //        emp.EmpDevSkills = EmployeeModel.DevSkills.TimeLogging;
+            //        break;
+            //    case EmployeeModel.DevSkills.TimeLogging:
+            //        emp.EmpDevSkills = EmployeeModel.DevSkills.TroubleShootingSkills;
+            //        break;
+            //    case EmployeeModel.DevSkills.TroubleShootingSkills:
+            //        return View("RateEmployeeSkills");
+
+            //}
+
+            return PartialView("_DevSkillType",emp);
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ EmployeeReview/EmployeeReview/Models/EmployeeModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeReview.Models
+{
+    public class EmployeeModel
+    {
+       
+        public string EmpName { get; set; }
+        public Designation EmpDesignation { get; set; }
+        public DevSkillScale EmpDevSkillScale { get; set; }
+        public static List<EmployeeModel> EmpDetails { get; set; }
+        public DevSkills EmpDevSkills { get; set; }
+
+        public enum DevSkills
+        {
+            CodingSkills,
+            TroubleShootingSkills,
+            QualityAssurance,
+            TimeLogging
+        }
+        public enum Designation
+        {
+            JuniorDeveloper,
+            SeniorDeveloper,
+            TeamLead,
+            TeamManager
+
+        }
+
+        public enum DevSkillScale
+        {
+            Unsatisfactory,
+            NeedsImprovement,
+            MeetsExpectations,
+            AboveExpectations,
+            Outstanding
+        }
+
+        static EmployeeModel()
+        {
+            EmpDetails = new List<EmployeeModel> { };
+        }
+    }
+}
