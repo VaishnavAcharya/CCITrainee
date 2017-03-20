694548c9b493bf465ae283ff0aff6cacef1d8ac3
VaishnavAcharya <vaishnavacharya@gmail.com>
2017-03-20 12:46:39 +0530

    HelloWorldConsoleApp

---
+++ HelloWorldConsoleApplication/HelloWorldConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            ConsoleKeyInfo keyinfo = Console.ReadKey(false);
+            Console.WriteLine(keyinfo.KeyChar);
+            Console.ReadKey();
+        }
+    }
+}
